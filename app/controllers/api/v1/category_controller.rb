7c3cf8e8516b1a4e9100e9c7db79d7561ad16327
Dave Marr <dave.marr@gmail.com>
2015-11-09 11:37:04 -0800

    Simpler render call

--- app/controllers/api/v1/category_controller.rb
+++ app/controllers/api/v1/category_controller.rb
@@ -2,8 +2,6 @@ class Api::V1::CategoryController < ApplicationController
   def show
     category = params[:category] ? params[:category] : "sports"
     @category = Category.where(:cat_title => category.capitalize).first
-    render :json => @category,
-    serializer: CategorySerializer,
-    root: "category"
+    render :json => @category
   end
 end
