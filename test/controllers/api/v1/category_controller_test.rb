2ddf17b7291903eb6bfcd1c67a5d8e7023539c26
Dave Marr <dave.marr@gmail.com>
2015-11-09 16:19:43 -0800

    add fixtures and tests

--- test/controllers/api/v1/category_controller_test.rb
+++ test/controllers/api/v1/category_controller_test.rb
@@ -2,8 +2,11 @@ require 'test_helper'
 
 class Api::V1::CategoryControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, {category: 'science'}
     assert_response :success
+    assert_equal "{\"category\":\
+{\"title\":\"science\",\
+\"sub_categories\":1}}",
+      @response.body
   end
-
 end
--- test/controllers/api/v1/graph_controller_test.rb
+++ test/controllers/api/v1/graph_controller_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class Api::V1::GraphControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, {category: 'sports'}
     assert_response :success
+    assert_equal "{\"sports\":\
+[{\"sub_category\":\
+\"culture_and_sports_culture\"}]}",
+      @response.body
   end
 
 end
